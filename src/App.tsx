c6515ec134493aadbda1ad308f691b78c67ec5fb
Evgeniy Klimovch <kliasanik@gmail.com>
2023-12-07 14:40:55 +0300

    localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,22 @@ function App() {
 
     const [value, setValue] = useState(0)
 
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+    const setToLocalStorageHandler = () => {
+
+    }
+    const getFromLocalStorageHandler = () => {
+
+    }
+
     return (
         <div className="App">
-            <h1>localstorage</h1>
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
         </div>
     );
 }
