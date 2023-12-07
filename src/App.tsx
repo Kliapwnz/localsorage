281f88984b65b2d7cea11151db59ae059d082589
Evgeniy Klimovch <kliasanik@gmail.com>
2023-12-07 14:30:46 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const [value, setValue] = useState(0)
 
 function App() {
-  return (
-    <div className="App">
-      <h1>localstorage</h1>
-    </div>
-  );
+
+    const [value, setValue] = useState(0)
+
+    return (
+        <div className="App">
+            <h1>localstorage</h1>
+        </div>
+    );
 }
 
 export default App;
